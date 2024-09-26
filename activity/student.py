2e458315ef4903b9334e409ce54012ce505522cc
anhtran <anhtr077@gmail.com>
2024-09-26 12:48:20 -0700

    Update files

--- activity/student.py
+++ activity/student.py
@@ -1,1 +1,17 @@
-# add your Student class here!
+class Student:
+    def __init__(self, name, grade, subjects):
+        self.name = name
+        self.grade = grade
+        self.subjects = subjects
+        
+    def add_class(self, subject):
+        self.subjects.append(subject)
+        
+    def get_num_classes(self):
+        return len(self.subjects)
+    
+    def summary(self):
+        print(f"{self.name} is a {self.grade} enrolled in {self.get_num_classes()} classes")
+        
+        
+        
